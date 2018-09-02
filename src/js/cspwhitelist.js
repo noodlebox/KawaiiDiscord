d5935c123ddfeb7cfa6557e95ac78eb2c34b59e6
noodlebox <ash@noodlebox.moe>
2018-09-01 19:32:15 -0500

    Build extension with gulp

---
+++ src/js/cspwhitelist.js
@@ -0,0 +1,23 @@
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details) {
+    const responseHeaders = details.responseHeaders.map(function(o) {
+        const name = o.name;
+        if (name !== "content-security-policy") {
+            return o;
+        }
+        let value = o.value;
+        value = value.replace(/img-src/i, "img-src https://smutba.se/media/emoji/ https://gitcdn.xyz/repo/jmhobbs/cultofthepartyparrot.com/ https://gitcdn.xyz/cdn/jmhobbs/cultofthepartyparrot.com/");
+        value = value.replace(/connect-src/i, "connect-src https://smutba.se/emoji/json/ https://gitcdn.xyz/repo/jmhobbs/cultofthepartyparrot.com/ https://gitcdn.xyz/cdn/jmhobbs/cultofthepartyparrot.com/");
+        return {name, value};
+    });
+    return {responseHeaders};
+  },
+  // filters
+  {
+    urls: [
+      "https://discordapp.com/*",
+      "https://canary.discordapp.com/*"
+    ],
+    types: ["main_frame"]
+  },
+  ["blocking", "responseHeaders"]);
